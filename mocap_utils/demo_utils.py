97f1d92dae407e582ddeeb8faa60e395c568b430
CK Madhavan <33689397+CkMadhavan@users.noreply.github.com>
2022-05-23 23:31:40 +0530

    Update demo_utils.py

--- mocap_utils/demo_utils.py
+++ mocap_utils/demo_utils.py
@@ -107,7 +107,59 @@ def setup_input(args):
         return input_type, cap
 
     elif input_type =='webcam':
-        cap = cv2.VideoCapture(0)       #webcam input
+
+        from IPython.display import display, Javascript
+        from google.colab.output import eval_js
+        from base64 import b64decode
+
+        def take_photo(filename='photo.jpg', quality=0.8):
+            js = Javascript('''
+                   async function takePhoto(quality) {
+            const div = document.createElement('div');
+            const capture = document.createElement('button');
+            capture.textContent = 'Capture';
+            div.appendChild(capture);
+
+            const video = document.createElement('video');
+            video.style.display = 'block';
+            const stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+            document.body.appendChild(div);
+            div.appendChild(video);
+            video.srcObject = stream;
+            await video.play();
+ 
+            // Resize the output to fit the video element.
+            google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+
+           // Wait for Capture to be clicked.
+           await new Promise((resolve) => capture.onclick = resolve);
+
+           const canvas = document.createElement('canvas');
+           canvas.width = video.videoWidth;
+           canvas.height = video.videoHeight;
+           canvas.getContext('2d').drawImage(video, 0, 0);
+           stream.getVideoTracks()[0].stop();
+           div.remove();
+           return canvas.toDataURL('image/jpeg', quality);
+           }
+           ''')
+          
+           display(js)
+           data = eval_js('takePhoto({})'.format(quality))
+           binary = b64decode(data.split(',')[1])
+           with open(filename, 'wb') as f:
+               f.write(binary)
+           return filename
+
+        try:
+            filename = take_photo()
+            cap = cv2.imread(filename)
+            
+        except Exception as err:
+            print(str(err))
+
+        #cap = cv2.VideoCapture(0)       #webcam input
         return input_type, cap
 
     elif input_type =='image_dir':
