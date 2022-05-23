a243a755c19837c0587dc2cd3b1dafbaf51babf6
CK Madhavan <33689397+CkMadhavan@users.noreply.github.com>
2022-05-24 00:35:38 +0530

    Update demo_utils.py

--- mocap_utils/demo_utils.py
+++ mocap_utils/demo_utils.py
@@ -83,47 +83,157 @@ def __img_seq_setup(args):
     mocap_out_dir = osp.join(args.out_dir, "mocap")
     gnu.build_dir(mocap_out_dir)
 
-def take_photo(filename='photo.jpg', quality=0.8):
-            js = Javascript('''
-                   async function takePhoto(quality) {
-            const div = document.createElement('div');
-            const capture = document.createElement('button');
-            capture.textContent = 'Capture';
-            div.appendChild(capture);
+import base64
+import html
+import io
+import time
+from base64 import b64decode
+from IPython.display import display, Javascript
+from google.colab.output import eval_js
+import numpy as np
+from PIL import Image
+import cv2
+from random import randint
 
-            const video = document.createElement('video');
-            video.style.display = 'block';
-            const stream = await navigator.mediaDevices.getUserMedia({video: true});
+def start_input():
+  js = Javascript('''
+    var video;
+    var div = null;
+    var stream;
+    var captureCanvas;
+    var imgElement;
+    var labelElement;
+    
+    var pendingResolve = null;
+    var shutdown = false;
+    
+    function removeDom() {
+       stream.getVideoTracks()[0].stop();
+       video.remove();
+       div.remove();
+       video = null;
+       div = null;
+       stream = null;
+       imgElement = null;
+       captureCanvas = null;
+       labelElement = null;
+    }
+    
+    function onAnimationFrame() {
+      if (!shutdown) {
+        window.requestAnimationFrame(onAnimationFrame);
+      }
+      if (pendingResolve) {
+        var result = "";
+        if (!shutdown) {
+          captureCanvas.getContext('2d').drawImage(video, 0, 0, 512, 512);
+          result = captureCanvas.toDataURL('image/jpeg', 0.8)
+        }
+        var lp = pendingResolve;
+        pendingResolve = null;
+        lp(result);
+      }
+    }
+    
+    async function createDom() {
+      if (div !== null) {
+        return stream;
+      }
 
-            document.body.appendChild(div);
-            div.appendChild(video);
-            video.srcObject = stream;
-            await video.play();
+      div = document.createElement('div');
+      div.style.border = '2px solid black';
+      div.style.padding = '3px';
+      div.style.width = '100%';
+      div.style.maxWidth = '600px';
+      document.body.appendChild(div);
+      
+      const modelOut = document.createElement('div');
+      modelOut.innerHTML = "<span>Status:</span>";
+      labelElement = document.createElement('span');
+      labelElement.innerText = 'No data';
+      labelElement.style.fontWeight = 'bold';
+      modelOut.appendChild(labelElement);
+      div.appendChild(modelOut);
+           
+      video = document.createElement('video');
+      video.style.display = 'block';
+      video.width = div.clientWidth - 6;
+      video.setAttribute('playsinline', '');
+      video.onclick = () => { shutdown = true; };
+      stream = await navigator.mediaDevices.getUserMedia(
+          {video: { facingMode: "environment"}});
+      div.appendChild(video);
+
+      imgElement = document.createElement('img');
+      imgElement.style.position = 'absolute';
+      imgElement.style.zIndex = 1;
+      imgElement.onclick = () => { shutdown = true; };
+      div.appendChild(imgElement);
+      
+      const instruction = document.createElement('div');
+      instruction.innerHTML =
+          '<span style="color: red; font-weight: bold;">' +
+          'When finished, click here or on the video to stop this demo</span>';
+      div.appendChild(instruction);
+      instruction.onclick = () => { shutdown = true; };
+      
+      video.srcObject = stream;
+      await video.play();
+
+      captureCanvas = document.createElement('canvas');
+      captureCanvas.width = 512; //video.videoWidth;
+      captureCanvas.height = 512; //video.videoHeight;
+      window.requestAnimationFrame(onAnimationFrame);
+      
+      return stream;
+    }
+    async function takePhoto(label, imgData) {
+      if (shutdown) {
+        removeDom();
+        shutdown = false;
+        return '';
+      }
+
+      var preCreate = Date.now();
+      stream = await createDom();
+      
+      var preShow = Date.now();
+      if (label != "") {
+        labelElement.innerHTML = label;
+      }
+            
+      if (imgData != "") {
+        var videoRect = video.getClientRects()[0];
+        imgElement.style.top = videoRect.top + "px";
+        imgElement.style.left = videoRect.left + "px";
+        imgElement.style.width = videoRect.width + "px";
+        imgElement.style.height = videoRect.height + "px";
+        imgElement.src = imgData;
+      }
+      
+      var preCapture = Date.now();
+      var result = await new Promise(function(resolve, reject) {
+        pendingResolve = resolve;
+      });
+      shutdown = false;
+      
+      return {'create': preShow - preCreate,
+              'show': preCapture - preShow,
+              'capture': Date.now() - preCapture,
+              'img': result};
+    }
+    ''')
+
+  display(js)
+  
+def take_photo(label, img_data):
+  data = eval_js('takePhoto("{}", "{}")'.format(label, img_data))
+  print(data)
+  binary = b64decode(data['img'])#.split(',')[1])
+  with open('hi.jpg', 'wb') as f:
+    f.write(binary)
+  return data
  
-            // Resize the output to fit the video element.
-            google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
-
-           // Wait for Capture to be clicked.
-           await new Promise((resolve) => capture.onclick = resolve);
-
-           const canvas = document.createElement('canvas');
-           canvas.width = video.videoWidth;
-           canvas.height = video.videoHeight;
-           canvas.getContext('2d').drawImage(video, 0, 0);
-           stream.getVideoTracks()[0].stop();
-           div.remove();
-           return canvas.toDataURL('image/jpeg', quality);
-           }
-           ''')
-          
-           display(js)
-           data = eval_js('takePhoto({})'.format(quality))
-           binary = b64decode(data.split(',')[1])
-           with open(filename, 'wb') as f:
-               f.write(binary)
-           return filename
-
-
 def setup_input(args):
     """
     Input type can be 
@@ -148,16 +258,21 @@ def setup_input(args):
 
     elif input_type =='webcam':
 
-        from IPython.display import display, Javascript
-        from google.colab.output import eval_js
-        from base64 import b64decode
-
-        try:
-            filename = take_photo()
-            cap = cv2.imread(filename)
+        start_input()
+        label_html = 'Capturing...'
+        img_data = ''
+        count = 0
+        
+        while True:
+           js_reply = take_photo(label_html, img_data)
+           if not js_reply:
+              break
+           try:
+              filename = take_photo()
+              cap = cv2.imread(filename)
             
-        except Exception as err:
-            print(str(err))
+           except Exception as err:
+              print(str(err))
 
         #cap = cv2.VideoCapture(0)       #webcam input
         return input_type, cap
